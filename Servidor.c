3593ab1a2980a58e023885e910b41077264a8e32
Pietro Pieri <p257337@dac.unicamp.br>
2024-04-03 08:58:28 -0300

    Initial commit

---
+++ Servidor.c
@@ -0,0 +1,8 @@
+/*  Alunos: Pietro Pieri - 257337
+           Francisco Guimarães - 
+    Disciplina: Lab de redes- MC833
+    Projeto 1:
+    Desenvolve programas com uso de socket para
+    comunicação cliente servidor
+    
+    SERVIDOR:*/
